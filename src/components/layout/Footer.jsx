25fd78ab8afdc2a18de9eecf1d6a53c2607e3bf1
oabnerwilliam <abnerwilliam026@gmail.com>
2025-04-02 13:27:52 -0300

    Estilizando o Footer

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -2,11 +2,26 @@ import React from 'react'
 
 import style from './Footer.module.css'
 
+import { FaInstagram, FaLinkedin, FaFacebook } from 'react-icons/fa'
+
 const Footer = () => {
   return (
-    <div>
-      
-    </div>
+    <>
+      <div className={style.footer}>
+        <div className={style.icones}>
+          <div className={style.icone}>
+            <FaInstagram/>
+          </div>
+          <div className={style.icone}>
+            <FaFacebook/>
+          </div>
+          <div className={style.icone}>
+            <FaLinkedin/>
+          </div>  
+        </div>
+        <p><strong>Notes&copy;</strong> 2025</p>
+      </div>
+    </>
   )
 }
 
