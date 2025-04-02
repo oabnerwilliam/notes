a5500860ba798435e9f95afd3b7bb7f2c46ecb4e
oabnerwilliam <abnerwilliam026@gmail.com>
2025-04-02 12:11:46 -0300

    Adicionando projeto Notes ao Git

---
+++ src/components/layout/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import style from './Navbar.module.css'
+
+const Navbar = () => {
+  return (
+    <div className={style.navbar}>
+        <h1>NOTES</h1>
+    </div>
+  )
+}
+
+export default Navbar
